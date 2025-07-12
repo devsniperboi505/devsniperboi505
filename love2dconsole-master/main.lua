7f294d573393e2b87936648df7b990b1723dae53
devsniperboi505 <sniperboi505@protonmail.com>
2025-07-12 13:16:23 +0800

    your file has been save Last Sync: 2025-07-12 13:16 gurt

--- love2dconsole-master/main.lua
+++ love2dconsole-master/main.lua
@@ -1,32 +1,26 @@
-
--- Example main file to show the console functionality.
-
-local console = require("loveconsole") -- Require the console script.
-
-function love.load()
-	-- Example of printing to the console with custom colors.
-	console.print("You are running LOVEConsole by Catlinman", {r = 0, g = 150, b = 255, a = 255})
-	console.warning("Wow what a warning. Something is about to break.")
-	console.error("Many error very scare.")
-	console.success("User is spooked.")
-
-	-- We define custom commands using the addCommand method.
-	-- A simple "Hello user" example command can be seen below.
-	console.addCommand("hello", function(args)
-		if args then
-			console.print(string.format("Greetings %s!", args[1]))	
-		else
-			console.print("Hey there!")
-		end
-	end, "Greets you in a non rude way - Arguments: [person to say hello to]")
-
-end
+-- main.lua
+ -- Require the console script and assign it to a variable.
+local console = require("loveconsole")
 
 function love.draw()
-	love.graphics.setBackgroundColor(10, 25, 50) -- Set a lame background.
+	-- [Do main drawing operations before drawing the console.]
+	love.graphics.pop() -- [Do this if you pushed any previous graphic translations.]
+	console.draw()
+end
 
-	-- Keep in mind to call love.graphics.pop() if you performed any translation changes using love.graphics.push()
-	console.draw() -- Draw the console at the end of the draw loop.
+function love.keypressed(key)
+	-- [Handle key presses and then pass these on to the console.]
+	console.keypressed(key)
+end
+
+function love.textinput(t)
+	-- [Receive text input and pass it on to the console.]
+	console.textinput(t)
+end
+
+function love.resize(w, h)
+	-- [Resize the console if the window is resized.]
+	console.resize(w, h)
 end
 
 function love.keypressed(key)
@@ -40,3 +34,76 @@ end
 function love.resize(w, h)
 	console.resize(w, h) -- Resize the console if the window is resized.
 end
+
+-- plugins zone
+love.graphics.setDefaultFilter("nearest", "nearest")
+
+function love.conf(t)
+    t.version = "0.10.0"
+    t.console = true
+end
+
+local console = require("loveconsole")
+-- varible zone
+local player = {
+    x = 100,
+    y = 100,
+    velocity = 0,
+    maxSpeed = 1100,
+    boostPerPress = 350,
+    friction = 5000
+}
+
+-- function code zone (you can code her)
+function love.keypressed(key)
+    console.keypressed(key)
+    if key == "d" then
+        isDHeld = true
+        player.velocity = math.min(player.velocity + player.boostPerPress, player.maxSpeed)
+    end
+end
+
+function love.textinput(t)
+    console.textinput(t)
+end
+
+function love.resize(w, h)
+    console.resize(w, h)
+end
+
+opt = require("settings")
+
+local sheet
+local Idle
+local idle2
+
+
+local isDHeld = false
+
+function love.keyreleased(key)
+    if key == "d" then isDHeld = false end
+end
+
+function love.load()
+    print("yo")
+    sheet = love.graphics.newImage("sprite/player.png")
+    Idle = love.graphics.newQuad(0, 0, 16, 16, sheet:getDimensions())
+end
+
+function love.update(dt)
+    if isDHeld == false then
+        player.velocity = math.max(0, player.velocity - player.friction * dt)
+    end
+
+    player.x = player.x + player.velocity * dt
+end
+
+function love.draw()
+    
+    opt.apply()
+    love.graphics.draw(sheet, Idle, player.x, player.y, nil, 3, 3)
+
+    print("yo")
+end
+
+
