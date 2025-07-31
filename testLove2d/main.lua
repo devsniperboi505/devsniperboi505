1b27b09b9b1b914538d225b63bf14924fe31eb72
devsniperboi505 <sniperboi505@protonmail.com>
2025-07-31 20:16:30 +0800

    your file has been save Last Sync: 2025-07-31 29PMe31 31o16plete

--- testLove2d/main.lua
+++ testLove2d/main.lua
@@ -19,17 +19,9 @@ log.fatal(...)
 ]]
 
 function love.load()
-  -- Log to a file
   log.outfile = "log.txt"
   player.load()
-  log.info('welcome to game engine love2d lightweight framework lua game engine if you want this log sytle go to https://github.com/rxi/log.lua and download')
-  log.dedug('yogurt')
-end
-
--- Custom output function
-log.out = function(str)
-  -- Custom handling of log string
-  my_custom_log_handler(str)
+  log.debug("hi")
 end
 
 function love.update(dt)
