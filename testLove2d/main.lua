cceec3a827ed84bcc78448a250bcfde1e4c21215
devsniperboi505 <sniperboi505@protonmail.com>
2025-08-19 21:41:10 +0800

    your file has been save Last Sync: 2025-08-19 9PMe19 19o41plete

--- testLove2d/main.lua
+++ testLove2d/main.lua
@@ -11,6 +11,28 @@ pie = piefiller:new()
 -- varible zone
 local drawPIE = false  -- Moved this to the top level scope
 -- function zone
+function love.load()
+	--This is one way of making a button. You can make it however you want, even use pre-made libraries
+	myButton = {
+		x = 10, y = 10, image = love.graphics.newImage("myButton.png"), clicked = false
+	}
+end
+
+function love.draw()
+  love.graphics.draw(myButton.image, myButton.x, myButton.y) --This is where we draw your button
+	
+  if myButton.clicked then --If the person clicked the button, this will be true
+    print("hi")
+  end
+end
+
+function love.mousepressed(x, y, button)
+	if button == 1 then --Left click
+		if x >= button.x and x <= button.x+button.image:getWidth() and y >= button.y and y <= button.y+button.image:getHeight() then --Detect if the click was inside the button
+			button.clicked = true --This is what triggers the pop-up image
+		end
+	end
+end
 
 function love.load()
   love.keypressed("f9")
@@ -37,6 +59,7 @@ function love.draw()
   pie:detach()
   if drawPIE then
     love.graphics.print(tostring(love.timer.getFPS( )), 1, 1, nil, 1.3, 1.3)
+    log.draw()
     pie:draw()
   end
   t.ends()
